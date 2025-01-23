ec22b27076e81b75a5ccb4fc20c51ad1eec41461
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-01-23 08:59:28 +0100

    chore(): update main nav

--- apps/docs/app/components/NavigationHeader.tsx
+++ apps/docs/app/components/NavigationHeader.tsx
@@ -46,7 +46,7 @@ const NavigationHeader = () => {
             </h2>
           </Link>
           <div className="hidden lg:flex gap-4 items-center">
-            <ul className="inline-flex h-9 items-center justify-center rounded-lg bg-muted-foreground p-1 text-muted">
+            <ul className="inline-flex h-9 items-center justify-center rounded-lg bg-primary-container p-1 text-primary">
               {['palette', 'examples', 'blocks', 'favourites'].map((item) => (
                 <NavLink
                   to={`/${item}`}
